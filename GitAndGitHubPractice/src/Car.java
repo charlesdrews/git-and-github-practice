7bb88f5c85ee5b8002a5b7d28f2178d17a46bdee
jafoole <smallbabba@gmail.com>
2016-01-14 12:30:18 -0500

    initial

--- GitAndGitHubPractice/src/Car.java
+++ GitAndGitHubPractice/src/Car.java
@@ -2,4 +2,12 @@
  * Created by charlie on 1/14/16.
  */
 public class Car {
+    public static void main(String[] args) {
+
+    }
+
+    public int numDoors;
+    public int maxSpeed;
+    public int minSpeed;
+
 }
